ba28a9b82c0db0e778bd233ca2458d014b3e8e2d
Anushka Shukla <anushka.shukla1708@gmail.com>
2026-07-30 03:26:22 +0530

    remove playsound and fix speech

--- data/speaker.py
+++ data/speaker.py
@@ -1,10 +1,16 @@
-import pyttsx3
-
-engine = pyttsx3.init()
+from gtts import gTTS
+import streamlit as st
+import tempfile
 
 
 def speak(text):
 
-    engine.say(text)
+    tts = gTTS(text=text, lang="en")
 
-    engine.runAndWait()
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
+        tts.save(fp.name)
+
+        audio_file = open(fp.name, "rb")
+        audio_bytes = audio_file.read()
+
+        st.audio(audio_bytes, format="audio/mp3")
